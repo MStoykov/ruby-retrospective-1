cb5a81bb62e129272dcd025693ab6eee2dbc5d55
Mihail Stoykov <m.stoikov@gmail.com>
2011-11-11 01:05:00 +0200

    `rake watch` said it was too big of an overkill so ...

--- solutions/01.rb
+++ solutions/01.rb
@@ -12,17 +12,9 @@ class Array
   end
 
   def subarray_count sub
-    return find_all_indexes(sub.first).select { |index| slice(index, sub.size) == sub }.size
-    return 0 if sub.length > length
-    buf = find_index(sub.first)
-    return 0 if buf == nil
-    [buf+1..-1].subarray_count(sub) + (self[buf, sub.length] == sub ? 1 : 0)
-  end
-
-  def find_all_indexes element
-    result = []
-    each_with_index { |item, index| result << index if item == element }
-    result
+    indexes = []
+    each_with_index { |item, index| indexes << index if item === sub.first }
+    indexes.select { |index| slice(index, sub.size) === sub }.size
   end
 
   def occurences_count
